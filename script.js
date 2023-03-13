8d6d1fd43d859911c0303c53f0d776adbf15cb2b
Bonnzalino <siwakorn.paorod@gmail.com>
2023-03-13 16:32:00 +0700

    Add script.js task1&2

---
+++ script.js
@@ -0,0 +1,30 @@
+//Create function following below
+
+function add(a,b) {
+    console.log("answer = " + (a + b));
+}
+
+function subtract(a,b) {
+    console.log("answer = " + (a - b));
+}
+
+function multiply(a,b) {
+    console.log("answer = " + a * b);
+}
+
+function divide(a,b) {
+    console.log("answer = " + (a / b));
+}
+
+add(15,3);      
+subtract(15,3);
+multiply(15,3);
+divide(15,3);
+
+//Create Function Inform
+
+function info(firstName, location, hobby) {
+    console.log("Hi, my name is " + firstName + ", I live in " + location + " and enjoy "+ hobby);
+}
+
+info('Bonn', 'Saraburi', 'Sleeping');
